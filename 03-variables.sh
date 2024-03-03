10667ea8b83ed50525cbf33f341ff8921cc0b879
girivardhanreddy <varahagiri234@gmail.com>
2024-03-03 21:36:21 +0530

    first commit

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Hello World, I am learning DevOps with AWS"
-PERSON1=$1 # this is the first value from command line
-PERSON2=$2 # # this is the second value from command line
+PERSON1=$suman # this is the first value from command line
+PERSON2=$ramesh # # this is the second value from command line
 
 echo "$PERSON1: Hi $PERSON2, Good Morning"
 echo "$PERSON2: Good Morning $PERSON1"
